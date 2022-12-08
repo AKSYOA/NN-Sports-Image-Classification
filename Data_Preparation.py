3b9d948195e5fb60dcccf3e5be06c322e1a10c91
sa3d <saadwaleed2019@gmail.com>
2022-12-08 16:39:32 +0200

    Reformat File Strctures

--- Data_Preparation.py
+++ Data_Preparation.py
@@ -6,41 +6,20 @@ import numpy as np
 data_path = 'data/'
 
 
-# Function to Get Data set
 def get_Dataset():
-    if os.path.exists('train_data.npy'):
-        train_data = np.load('train_data.npy', allow_pickle=True)
-    else:
-        train_data, test_data = create_Dataset()
-
-    if os.path.exists('test_data.npy'):
-        test_data = np.load('test_data.npy', allow_pickle=True)
-    else:
-        train_data, test_data = create_Dataset()
+    train_data = []
+    test_data = []
+    for type_folder in os.listdir(data_path):
+        if type_folder == 'Train':
+            train_data = read_images(data_path + type_folder)
+        else:
+            test_data = read_images(data_path + type_folder)
 
     X_train, Y_train = reformat_dataset(train_data)
     X_test, Y_test = reformat_dataset(test_data)
     return X_train, Y_train, X_test, Y_test
 
 
-# Function to Create Data Set
-def create_Dataset():
-    train_data = []
-    test_data = []
-    for type_folder in os.listdir(data_path):
-        if type_folder == 'Train':
-            train_data = read_images(data_path + type_folder)
-            shuffle(train_data)
-            np.save('train_data.npy', train_data)
-        else:
-            test_data = read_images(data_path + type_folder)
-            shuffle(test_data)
-            np.save('test_data.npy', test_data)
-
-    return train_data, test_data
-
-
-# Function to read images with their path
 def read_images(images_paths):
     images = []
     for i in os.listdir(images_paths):
@@ -52,12 +31,10 @@ def read_images(images_paths):
     return images
 
 
-# Function to resize the Images
 def resize_image(image, image_size):
     return cv2.resize(image, (image_size, image_size))
 
 
-# Function to Labelled Images
 def create_label(image_path):
     image_label = image_path.split('_')[0]
     image_classes = ['Basketball', 'Football', 'Rowing', 'Swimming', 'Tennis', 'Yoga']
@@ -68,7 +45,6 @@ def create_label(image_path):
     return label_encoded
 
 
-# Function to reformat the Data Set
 def reformat_dataset(data):
     X = np.array([i[0] for i in data], dtype=object)
     Y = np.array([i[1] for i in data])
